751cf656fd216ae4e4a0a46df7eb5319b7fb7d4b
shouya <idb8bzputt@gmail.com>
2020-10-07 20:07:45 +0900

    MongoDBとの接続処理を実装。

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
-const graphqlHTTP = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect(
+  'mongodb+srv://shoya_kosuge:0082650@cluster0.qsmtm.mongodb.net/<dbname>?retryWrites=true&w=majority'
+)
+mongoose.connection.once('open', () => {
+  console.log('db connected')
+})
 
 app.use('/graphql', graphqlHTTP({}))
 
