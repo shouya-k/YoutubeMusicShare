56170485877910d9c64eb59c919b646ec7120e4b
shouya <idb8bzputt@gmail.com>
2020-10-08 19:17:23 +0900

    GraphQLでデータの取得、追加する処理を実装。

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const mongoose = require('mongoose')
+const schema = require('./schema/schema')
 const app = express()
 
 mongoose.connect(
-  'mongodb+srv://shoya_kosuge:0082650@cluster0.qsmtm.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  'mongodb+srv://shoya_kosuge:0082650@cluster0.a6eh3.mongodb.net/posts?retryWrites=true&w=majority'
 )
 mongoose.connection.once('open', () => {
   console.log('db connected')
 })
 
-app.use('/graphql', graphqlHTTP({}))
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+)
 
 app.listen(4000, () => {
   console.log('listening port 4000')
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,23 +1,52 @@
 const graphql = require('graphql')
+const Music = require('../models/music')
 
-const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = graphql
 
-const MovieType = new GraphQLObjectType({
-  name: 'Movie',
+const MusicType = new GraphQLObjectType({
+  name: 'Music',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString },
+    category: { type: GraphQLString },
   }),
 })
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    movie: {
-      type: MovieType,
-      args: { id: { type: GraphQLString } },
-      resolve(parents, args) {},
+    music: {
+      type: MusicType,
+      args: { id: { type: GraphQLID } },
+      resolve(parents, args) {
+        return Music.findById(args.id)
+      },
     },
   },
+})
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addMusic: {
+      type: MusicType,
+      args: {
+        name: { type: GraphQLString },
+        category: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        let music = new Music({
+          name: args.name,
+          category: args.category,
+        })
+
+        return music.save()
+      },
+    },
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
 })
