d6c8b909c3d9a24f212b2afbf775d39c5626bbf9
shouya <idb8bzputt@gmail.com>
2020-10-09 19:09:24 +0900

    header.vue, timeline.vueを追加。

---
+++ components/header.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="head">
+    <h1 class="head__logo">Youtube Music Share</h1>
+
+    <nav class="nav" :class="{ 'nav--show': isNavShow }">
+      <div class="nav__icon" @click="navHidden">
+        <i class="fas fa-times nav__times-icon"></i>
+      </div>
+      <ul class="nav__menu">
+        <li class="nav__item">
+          <a href="./my-page.html" class="nav__link">マイページ</a>
+        </li>
+        <li class="nav__item">
+          <a href="index.html" class="nav__link nav__link--active"
+            >タイムライン</a
+          >
+        </li>
+        <li class="nav__item">
+          <a href="#" class="nav__link">カテゴリ</a>
+        </li>
+        <li class="nav__item">
+          <a href="#" class="nav__link">ランキング</a>
+        </li>
+        <li class="nav__item nav__item--logout">
+          <a href="#" class="nav__link">ログアウト</a>
+        </li>
+      </ul>
+    </nav>
+    <button type="button" class="head__logout-button">ログアウト</button>
+    <div class="nav__icon" @click="navShow">
+      <i class="fas fa-bars fa-2x" style="color: #fff"></i>
+    </div>
+    <div
+      class="nav__bg"
+      :class="{ 'nav__bg--show': isNavShow }"
+      @click="navHidden"
+    ></div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isNavShow: false,
+    }
+  },
+  methods: {
+    navShow() {
+      this.isNavShow = true
+    },
+    navHidden() {
+      this.isNavShow = false
+    },
+  },
+}
+</script>
---
+++ pages/timeline.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import header from '../components/header'
+export default {
+  components: {
+    Header: header,
+  },
+}
+</script>
